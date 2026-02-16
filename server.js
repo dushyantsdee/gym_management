ea20d9dd55711e3b58365e185ad08fe594833dcd
Dushyant Gaur <dushyant.gaur_cs25@gla.ac.in>
2026-02-16 15:30:18 +0530

    Fixed Cloudinary config and duplicate multer

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const session = require("express-session")
 const path = require("path")
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const multer = require("multer");
+
 
 
 const app = express()
