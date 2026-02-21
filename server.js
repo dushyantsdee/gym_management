3eed31faba362f32234d984b9f30a81b8b7d8f50
Dushyant Gaur <dushyant.gaur_cs25@gla.ac.in>
2026-02-21 17:50:08 +0530

    Fixed MongoDB connection options

--- server.js
+++ server.js
@@ -102,16 +102,15 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(`Error: ${err.message}`);
     process.exit(1);
   }
 };
+
+connectDB();
 
 connectDB();
 
